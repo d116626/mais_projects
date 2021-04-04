c9e475aaed4ca43fc3b7a3ef1bdc4d1f5e521869
d116626 <d116626@gmail.com>
2021-04-04 02:45:10 -0300

    all

--- notebooks/run_caged.py
+++ notebooks/run_caged.py
@@ -21,24 +21,69 @@ import glob
 
 
 def main():
-    tb = Table("microdados_antigo", "br_me_caged")
+    # ### Padroniza CAGED ANTIGO AJUSTES
+    # clean_save_path = "../data/caged/clean/caged_antigo_ajustes/"
+    # folders = glob.glob("../data/caged/raw/caged_antigo_ajustes/*/*/")
+    # municipios = pd.read_parquet("../data/caged/raw/municipios.parquet")
 
-    # tb.create(
-    #     path='../data/caged/clean/caged_antigo/2019/',
-    #     if_table_exists='replace',
-    #     if_storage_data_exists='replace',
-    #     if_table_config_exists='pass'
+    # caged.caged_antigo_padronize_and_partitioned(
+    #     folders, clean_save_path, municipios, force_remove_csv=False
     # )
 
-    for ano in range(2014, 2019):
-        tb.append(
-            filepath=f"../data/caged/clean/caged_antigo/{ano}/",
-            partitions="ano=value/mes=value/sigla_uf=value",
-            if_exists="replace",
-        )
-        print(f"{ano} done!\n")
+    ### Padroniza CAGED ANTIGO
+    clean_save_path = "../data/caged/clean/caged_antigo/"
+    folders = glob.glob("../data/caged/raw/caged_antigo/*/*/")
+    municipios = pd.read_parquet("../data/caged/raw/municipios.parquet")
 
-    tb.publish(if_exists="replace")
+    caged.caged_antigo_padronize_and_partitioned(
+        folders, clean_save_path, municipios, force_remove_csv=False
+    )
+
+    # # CAGED ANTIGO AJUSTES
+
+    # tb = Table("microdados_antigo_ajutes", "br_me_caged")
+
+    # tb.create(
+    #     path="../data/caged/clean/caged_antigo_ajustes/2019/",
+    #     if_table_exists="replace",
+    #     if_storage_data_exists="pass",
+    #     if_table_config_exists="pass",
+    # )
+
+    # tb.publish(if_exists="replace")
+
+    # for ano in range(2007, 2019):
+    #     tb.append(
+    #         filepath=f"../data/caged/clean/caged_antigo_ajustes/{ano}/",
+    #         partitions="ano=value/mes=value/sigla_uf=value",
+    #         if_exists="replace",
+    #     )
+    #     print(f"{ano} done!\n")
+
+    # # tb.publish(if_exists='replace')
+
+    # # CAGED ANTIGO PADRAO
+
+    # tb = Table("microdados_antigo", "br_me_caged")
+
+    # tb.create(
+    #     path="../data/caged/clean/caged_antigo/2019/",
+    #     if_table_exists="replace",
+    #     if_storage_data_exists="replace",
+    #     if_table_config_exists="pass",
+    # )
+
+    # tb.publish(if_exists="replace")
+
+    # for ano in range(2007, 2019):
+    #     tb.append(
+    #         filepath=f"../data/caged/clean/caged_antigo/{ano}/",
+    #         partitions="ano=value/mes=value/sigla_uf=value",
+    #         if_exists="replace",
+    #     )
+    #     print(f"{ano} done!\n")
+
+    # tb.publish(if_exists="replace")
 
 
 if __name__ == "__main__":
